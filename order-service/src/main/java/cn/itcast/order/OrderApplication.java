93541704567ed23a4534572a39f7412d4a9cb831
Haolijia <1580766934@qq.com>
2023-05-20 16:24:06 +0800

    cloud-demo-nacos

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,39 @@
+package cn.itcast.order;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    /**
+     * 创建RestTemplate并注入spring容器
+     * 作用:使程序能够发送http请求
+     * @return
+     */
+    @Bean
+    @LoadBalanced   /*负载均衡:默认轮询*/
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    /**
+     * 将负载均衡规则改为随机(针对全体服务)----方法1
+     * @return
+     */
+    /*@Bean
+    public IRule randomRule(){
+        return new RandomRule();
+    }*/
+}
---
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -0,0 +1,36 @@
+package cn.itcast.order.web;
+
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import cn.itcast.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+   @Autowired
+   private OrderService orderService;
+
+   @Autowired
+   private RestTemplate restTemplate;
+
+    @GetMapping("{orderId}")
+    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+        //1,根据id查询订单
+        Order order=orderService.queryOrderById(orderId);
+
+        //2,服务名称代替地址
+        String url="http://userservice/user/"+order.getUserId();
+                                                 //返回值类型
+        User user = restTemplate.getForObject(url, User.class);
+        //3，封装user到order中
+        order.setUser(user);
+        return order;
+    }
+}
