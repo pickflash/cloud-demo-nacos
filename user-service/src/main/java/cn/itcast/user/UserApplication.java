951236d3dbaf4e356971102902b72e72bc888c88
Haolijia <1580766934@qq.com>
2023-05-20 20:21:55 +0800

    为服务添加集群以及命名空间

--- user-service/src/main/java/cn/itcast/user/UserApplication.java
+++ user-service/src/main/java/cn/itcast/user/UserApplication.java
@@ -1,8 +1,10 @@
 package cn.itcast.user;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 
 @MapperScan("cn.itcast.user.mapper")
 @SpringBootApplication
